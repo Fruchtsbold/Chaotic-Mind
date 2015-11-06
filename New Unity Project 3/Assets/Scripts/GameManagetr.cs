87d32e447fe68f31eff8aa4567200e882f54ebed
Fruchtsbold <m.ehlmaier@aon.at>
2015-11-06 14:05:40 +0100

    First
    First

---
+++ New Unity Project 3/Assets/Scripts/GameManagetr.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagetr : MonoBehaviour {
+
+    public bool phase1;
+    public bool phase2;
+    public bool phase3;
+
+
+    void Start()
+    {
+        phase1 = true;
+        phase2 = false;
+        phase3 = false;
+    }
+	
+	void Update () {
+
+        if (phase1)
+        { 
+            if(this.gameObject.transform.position.y < -3)
+            {
+                Application.LoadLevel("a");
+            }
+        }
+        else if (phase2)
+        {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("Platform");
+            for (int i=0; i < objs.Length; i++ )
+            {
+                objs[i].GetComponent<Platform>().phase2 = true;
+            }
+        }
+
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Platform" && phase2)
+        {
+            Application.LoadLevel("a");
+        }
+
+        if(collision.collider.tag.Equals("ChangeTrigger"))
+        {
+            Destroy(collision.collider.gameObject);
+            phase1 = false;
+            phase2 = true;
+        }
+    }
+
+}
---
+++ New Unity Project 3/Assets/Scripts/Platform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+
+    public GameObject player;
+    public bool phase2;
+    public float speed;
+    public float triggerDistance;
+
+    private Vector3 playerpos;
+    private Vector3 mypos;
+
+    void Start () {
+        phase2 = false;
+        mypos = this.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (phase2)
+        {
+            playerpos = player.transform.position;
+
+            Vector3 direction = playerpos - mypos;
+            Debug.Log(direction.magnitude);
+            if(direction.magnitude < triggerDistance)
+            {
+                this.gameObject.transform.Translate(direction.normalized * speed);
+
+            }
+        }
+	}
+}
